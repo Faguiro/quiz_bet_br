b1077b97931f301d97bc50bfd7db969e0b28e243
faguiro <faguiro2005@gmail.com>
2023-04-23 22:08:02 -0300

    :bricks: Refatoração

--- bet/main/routes.py
+++ bet/main/routes.py
@@ -16,6 +16,7 @@ import  stripe
 import mercadopago
 from bet import db, Config
 import os
+from jinja2 import Template, UndefinedError
 
 
 
@@ -77,23 +78,27 @@ def explore():
 
 
 @bp.route('/user/<username>')
-@login_required
+#@login_required
 def user(username):
-    user = User.query.filter_by(username= username).first_or_404()
+    if current_user.is_authenticated:
+        user = User.query.filter_by(username= username).first_or_404()
+    else:
+        user = User.query.filter_by(username= "fabio").first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=current_app.config['POSTS_PER_PAGE'],
         error_out=False)
     next_url = url_for('main.user', username=user.username,
-                       page=posts.next_num) if posts.has_next else None
+                    page=posts.next_num) if posts.has_next else None
     prev_url = url_for('main.user', username=user.username,
-                       page=posts.prev_num) if posts.has_prev else None
+                    page=posts.prev_num) if posts.has_prev else None
     form = EmptyForm()
+    
     saldo = str(user.saldo) # obtém o saldo do usuário
     return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url, form=form, saldo=saldo)
+                        next_url=next_url, prev_url=prev_url, form=form, saldo=saldo)
 
-
+ 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
